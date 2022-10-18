e04839f17ab6e7186186a9c45ab15bf7d08c2570
Adrian Chojnacki <chojnackiadrian1@gmail.com>
2022-10-18 13:09:34 +0200

    Add todos render

--- src/features/todos/TodoList.js
+++ src/features/todos/TodoList.js
@@ -1,5 +1,10 @@
 import { useState } from "react";
-import { useGetTodosQuery } from "../api/apiSlice";
+import {
+  useGetTodosQuery,
+  useAddTodoMutation,
+  useUpdateTodoMutation,
+  useDeleteTodoMutation,
+} from "../api/apiSlice";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash, faUpload } from "@fortawesome/free-solid-svg-icons";
 
@@ -13,10 +18,13 @@ const TodoList = () => {
     isError,
     error,
   } = useGetTodosQuery();
+  const [addTodo] = useAddTodoMutation();
+  const [updateTodo] = useUpdateTodoMutation();
+  const [deleteTodo] = useDeleteTodoMutation();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    //addTodo
+    addTodo({ userId: 1, title: newTodo, completed: false });
     setNewTodo("");
   };
 
@@ -42,7 +50,24 @@ const TodoList = () => {
   if (isLoading) {
     content = <p>Loading...</p>;
   } else if (isSuccess) {
-    content = JSON.stringify(todos);
+    content = todos.map((todo) => (
+      <article key={todo.id}>
+        <div className='todo'>
+          <input
+            type='checkbox'
+            checked={todo.completed}
+            id={todo.id}
+            onChange={() =>
+              updateTodo({ ...todos, completed: !todo.completed })
+            }
+          />
+          <label htmlFor={todo.id}>{todo.title}</label>
+        </div>
+        <button className='trash' onClick={() => deleteTodo({ id: todo.id })}>
+          <FontAwesomeIcon icon={faTrash} />
+        </button>
+      </article>
+    ));
   } else if (isError) {
     content = <p>{error}</p>;
   }
